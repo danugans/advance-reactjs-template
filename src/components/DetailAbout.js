8e764867b3885f37bebd42e52976ac616d783677
danugans <mas.danu29@gmail.com>
2022-02-04 08:56:39 +0700

    update

--- src/components/DetailAbout.js
+++ src/components/DetailAbout.js
@@ -1,5 +1,22 @@
 // import package for property types
+import PropTypes from "prop-types";
 
 // create component here
+import React from "react";
 
 // define property type for this component here
+const DetailAbout = (props) => {
+  return (
+    <>
+      <h1>title: {props.title}</h1>
+      <p>summary: {props.summary}</p>
+      <p>total guests: {props.total}</p>
+    </>
+  );
+};
+
+DetailAbout.propTypes = {
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string,
+  total: PropTypes.string,
+};
